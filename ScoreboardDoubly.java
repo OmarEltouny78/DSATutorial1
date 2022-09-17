32b9d4f9de2b68bc6afd53e9523dea9eca029ae9
Omar Eltouny <60698807+OmarEltouny78@users.noreply.github.com>
2022-09-17 11:10:23 +0200

    JavaScoreboard

---
+++ ScoreboardDoubly.java
@@ -0,0 +1,14 @@
+public class ScoreboardDoubly {
+    private int numEntries;
+    private GameEntry[]board;
+
+    public ScoreboardDoubly(int capacity) {
+        board=new GameEntry[capacity];
+    }
+    public void add(GameEntry e){
+
+    }
+    // public GameEntry remove(int i){
+    //}
+}
+
---
+++ ScoreboardSingly.java
@@ -0,0 +1,13 @@
+public class ScoreboardSingly {
+    private int numEntries;
+    private GameEntry[]board;
+
+    public ScoreboardSingly(int capacity) {
+        board=new GameEntry[capacity];
+    }
+    public void add(GameEntry e){
+
+    }
+   // public GameEntry remove(int i){
+    //}
+}
